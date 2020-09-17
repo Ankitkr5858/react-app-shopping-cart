80bae6eddcb643c3079d2734e2923b67bb896612
Ankit Kumar <ankitkumar@192.168.1.3>
2020-09-17 18:56:20 +0530

    shopping cart

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import { GitHubIcon} from '../components/icons'
+
+const About = () => {
+    
+    return ( 
+        <Layout title="About" description="This is the About page" >
+            <div className="text-center mt-5">
+                <h1>About</h1>
+                <a className="btn btn-primary" href="">
+                    <GitHubIcon width={"18px"}/> <span className="ml-2 mr-4">Visit Repo</span></a>
+            </div>
+        </Layout>
+     );
+}
+ 
+export default About;
